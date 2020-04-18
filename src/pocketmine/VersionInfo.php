27e8075db29b2274dc90279a8eb2154b5be95ef9
HowToPeaZYT | Florian <50436997+HowToPeaZYT@users.noreply.github.com>
2020-04-18 12:47:43 +0200

    Update VersionInfo.php

--- src/pocketmine/VersionInfo.php
+++ src/pocketmine/VersionInfo.php
@@ -34,5 +34,5 @@ const _VERSION_INFO_INCLUDED = true;
 
 const NAME = "Altay";
 const BASE_VERSION = "3.11.8";
-const IS_DEVELOPMENT_BUILD = true;
+const IS_DEVELOPMENT_BUILD = false;
 const BUILD_NUMBER = 0;
